579356eedccb62c9e1f00f742657b250b1fb7667
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-07-19 14:10:00 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
-
+import React, { useState } from "react";
+// import Styles from './styles';
+// import Title from "./title";
+// import Todos from "./todos";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[ newEntry, setNewEntry ] = useState ('');
+	const[ taskList, setNewList ] = useState ([]);
+
+	function handleSubmit(event) {
+		event.preventDefault ();
+		
+	}
+console.log(taskList);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-50">
+			<h1>Todos</h1>
+			<form onSubmit={handleSubmit}>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail" className="from-label"></label>
+					<input 
+						placeholder="What needs to be done?"
+						onChange={(event) => setNewEntry(event.target.value)}
+						onKeyDown={(event)=>{
+							if (event.key==='Enter')
+								{
+									setNewList (taskList.concat([newEntry]))
+									setNewEntry ('')
+								}
+						} }
+						value={newEntry}
+					/>
+				</div>
+			
+			</form>
+			<ul className="todo">
+				{
+					taskList.map((task, index)=>{
+						return(
+
+							<li key={index}>{task}{' '}<i onClick={()=>{setNewList(
+								taskList.filter((t, currentIndex)=> {return(
+									index != currentIndex
+								)})
+							)} } className="fa-solid fa-trash"></i> </li>
+						)
+					})
+				}
+
+			</ul>
 		</div>
-	);
+	
+		);
 };
 
 export default Home;
---
+++ src/js/component/todos.jsx
@@ -0,0 +1,10 @@
+		
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>Title</div>
+  )
+}
+
+export default Card
